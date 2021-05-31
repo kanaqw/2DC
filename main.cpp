f5a85e51ad981e35b73b974a1fd7cf9fd177c967
kanaqw <81157913+kanaqw@users.noreply.github.com>
2021-05-31 13:57:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,34 @@
 #include <cmath>
 #include <stdio.h>
 #include <string>
-
+#include <sstream>
+//TODO create new renderer for each window
 
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 600;
+
+class Dot {
+public:
+	//dot dimensions
+	static const int DOT_WIDTH = 20;
+	static const int DOT_HEIGHT = 20;
+	//Maximum axis velocity of the dot
+	static const int DOT_VEL = 10;
+	//Init the variables
+	Dot();
+	//Takes key presses and adjusts the dot velocity
+	void handleEvent(SDL_Event& e);
+	//Moves the dot
+	void move(SDL_Rect& wall);
+	//shows the dot on screen
+	bool render(bool dotRenderFlag);
+	//The X and Y offsets of the dot
+	int mPosX, mPosY;
+	//velocity of the dot
+	int mVelX, mVelY;
+private: 
+	SDL_Rect mCollider;
+};
 
 class LTexture {
 public:
@@ -55,6 +79,8 @@ bool init();
 bool loadMedia();
 //Frees media and shuts down SDL
 void close();
+//Box collision detector
+bool checkCollision(SDL_Rect a, SDL_Rect b);
 /////////////////////////////////////////////GLOBAL VARIABLES///////////////////////////////////////////////////
 //Loads individual texture as image
 SDL_Texture* loadTexture(std::string path);
@@ -62,25 +88,37 @@ SDL_Surface* loadSurface(std::string path);
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 //The window renderer
-SDL_Renderer* gRenderer = NULL;
+SDL_Renderer* gRendererMain = NULL;
+//Left map renderer
+SDL_Renderer* gRendererLeft = NULL;
+//Right map renderer
+SDL_Renderer* gRendererRight = NULL;
 //Globally used font
 TTF_Font* gFont = NULL;
 //Current displayed texture
 SDL_Texture* gTexture = NULL;
 //Map texture
 LTexture gMapLeft;
+LTexture gMapRight;
 //text block texture
 LTexture gTextBlock;
 //Keys anim pointer
 SDL_Texture* gKeys = NULL;
-SDL_Texture* gSeederIcon = NULL;
+SDL_Texture* gSeederIconLeft = NULL;
+SDL_Texture* gSeederIconRight = NULL;
 //the image that correspond to a keypress
 SDL_Texture* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
 //Walking animation
 const int WALKING_ANIMATION_FRAMES = 4;
-SDL_Rect gSpriteClips[WALKING_ANIMATION_FRAMES];
-LTexture gSpriteSheetTexture;
+SDL_Rect gSpriteClipsLeft[WALKING_ANIMATION_FRAMES];
+SDL_Rect gSpriteClipsRight[WALKING_ANIMATION_FRAMES];
+//Textures
+LTexture gSeederIconTexture;
+LTexture gSeederMiniIconTexture;
 LTexture gMapTexture;
+LTexture gDotTexture;
+LTexture gTimeTextTexture;
+LTexture gPromptTextTexture;
 ///////////////////////////////////////////////END OF GV//////////////////////////////////////////////////////////
 LTexture::LTexture() {
 	//Initialize
@@ -106,7 +144,7 @@ bool LTexture::loadFromFile(std::string path) {
 	}
 	else {
 		//Create image from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		newTexture = SDL_CreateTextureFromSurface(gRendererMain, loadedSurface);
 		if (newTexture == NULL) {
 			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
 		}
@@ -129,7 +167,7 @@ bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor
 	}
 	else {
 		//Create texture from surface pixels
-		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+		mTexture = SDL_CreateTextureFromSurface(gRendererMain, textSurface);
 		if (mTexture == NULL) {
 			printf("Unable to create texture from rendered text! SDL_Error%s\n", SDL_GetError());
 		}
@@ -173,7 +211,7 @@ void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* cen
 		renderQuad.w = clip->w;
 		renderQuad.h = clip->h;
 	}
-	SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
+	SDL_RenderCopyEx(gRendererMain, mTexture, clip, &renderQuad, angle, center, flip);
 }
 
 int LTexture::getWidth() {
@@ -183,6 +221,107 @@ int LTexture::getWidth() {
 int LTexture::getHeight() {
 	return mHeight;
 }
+
+Dot::Dot() {
+	//init offsets
+	mPosX = 275;
+	mPosY = 460;
+	//Set collision box dimension
+	mCollider.w = DOT_WIDTH;
+	mCollider.h = DOT_HEIGHT;
+	//init velocity
+	mVelX = 0;
+	mVelY = 0;
+}
+
+void Dot::handleEvent(SDL_Event& e) {
+	//if a key was pressed
+	if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+		//Adjust the velocity
+		switch (e.key.keysym.sym) {
+		case SDLK_UP: mVelY -= DOT_VEL; break;
+		case SDLK_DOWN:mVelY += DOT_VEL; break;
+		case SDLK_LEFT: mVelX -= DOT_VEL; break;
+		case SDLK_RIGHT: mVelX += DOT_VEL; break;
+		}
+	}
+	//If a key was released 
+	else if (e.type == SDL_KEYUP && e.key.repeat == 0) {
+		//Adjust the velocity
+		switch (e.key.keysym.sym) {
+		case SDLK_UP: mVelY += DOT_VEL; break;
+		case SDLK_DOWN:mVelY -= DOT_VEL; break;
+		case SDLK_LEFT: mVelX += DOT_VEL; break;
+		case SDLK_RIGHT: mVelX -= DOT_VEL; break;
+		}
+	}
+}
+
+void Dot::move(SDL_Rect& wall) {
+	//Move the dot left or right. Velocity 5x lesser
+	mPosX += mVelX/5;
+	mCollider.x = mPosX;
+	//if the dot went too far to left or right
+	if ((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH)|| checkCollision(mCollider, wall)) {
+		//move back
+		mPosX -= mVelX/5;
+		mCollider.x = mPosX;
+	}
+	//Move the dot up or down
+	mPosY += mVelY/5;
+	mCollider.y = mPosY;
+	//if the dot went too far up or down
+	if ((mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT)|| checkCollision(mCollider, wall)) {
+		//Move back
+		mPosY -= mVelY/5;
+		mCollider.y = mPosY;
+	}
+}
+
+bool checkCollision(SDL_Rect a, SDL_Rect b) {
+	int leftA, leftB;
+	int rightA, rightB;
+	int topA, topB;
+	int bottomA, bottomB;
+
+	//Calculate the sides of rect A
+	leftA = a.x;
+	rightA = a.x + a.w;
+	topA = a.y;
+	bottomA = a.y + a.h;
+	//Calculate the sides of rect B
+	leftB = b.x;
+	rightB = b.x + b.w;
+	topB = b.y;
+	bottomB = b.y + b.h;
+
+	//if any of the sides from A are outside of B
+	if (bottomA <= topB) {
+		return false;
+	}
+	if (topA >= bottomB) {
+		return false;
+	}
+	if (rightA <= leftB) {
+		return false;
+	}
+	if (leftA >= rightB) {
+		return false;
+	}
+	//If none of the sides from A are outside B
+	return true;
+
+}
+
+bool Dot::render(bool dotRenderFlag) {
+	//Show the dot
+	if (dotRenderFlag) {
+		gDotTexture.render(mPosX, mPosY);
+
+		return dotRenderFlag;
+	}
+}
+
 
 bool init() {
 	//init flag
@@ -205,8 +344,8 @@ bool init() {
 		}
 		else {
 			//initialize png loading
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED| SDL_RENDERER_PRESENTVSYNC);
-			if (gRenderer == NULL) {
+			gRendererMain = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED| SDL_RENDERER_PRESENTVSYNC);
+			if (gRendererMain == NULL) {
 				printf("Renderer could not be created! SDL Error %s\n", SDL_GetError());
 				success = false;
 			}
@@ -222,7 +361,7 @@ bool init() {
 						success = false;
 					}
 					//Init renderer color
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_SetRenderDrawColor(gRendererMain, 0xFF, 0xFF, 0xFF, 0xFF);
 				
 			}
 		}
@@ -233,6 +372,11 @@ bool init() {
 bool loadMedia() {
 	//loading success flag
 	bool success = true;
+	//Load dot texture
+	if (!gDotTexture.loadFromFile("SeederIconMini.png")) {
+		printf("Failed to load dot texture\n");
+		success = false;
+	}
 	//open the font
 	gFont = TTF_OpenFont("LTYPE.TTF", 18);
 	if (gFont == NULL) {
@@ -242,8 +386,8 @@ bool loadMedia() {
 	else {
 		//Render text
 		SDL_Color textColor{ 255,0,0 };
-		std::string Text = "Text check";
-		if (!gTextBlock.loadFromRenderedText(Text,textColor ))
+		std::string Text = "Press Enter to Reset Start time.";
+		if (!gTextBlock.loadFromRenderedText(Text, textColor))
 		{
 			printf("Failed to render text texture!\n");
 			success = false;
@@ -256,22 +400,37 @@ bool loadMedia() {
 		success = false;
 	}
 	//Load sprite animation texture
-	if (!gSpriteSheetTexture.loadFromFile("SeederIconTexture.png")) {
-		printf("Failed to load move animation texture!\n");
+	if (!gSeederIconTexture.loadFromFile("SeederIconTexture.png")) {
+		printf("Failed to load left icon animation texture!\n");
 		success = false;
 	}
 	else {
-		gSpriteClips[0].x = 4; gSpriteClips[0].y = 0; gSpriteClips[0].w = 134; gSpriteClips[0].h = 99;
-
-		gSpriteClips[1].x = 4; gSpriteClips[1].y = 105; gSpriteClips[1].w = 134; gSpriteClips[1].h = 99;
-
-		gSpriteClips[2].x = 4; gSpriteClips[2].y = 210; gSpriteClips[2].w = 134; gSpriteClips[2].h = 99;
-
-		gSpriteClips[3].x = 4; gSpriteClips[3].y = 315; gSpriteClips[3].w = 134; gSpriteClips[3].h = 99;
+		gSpriteClipsLeft[0].x = 4; gSpriteClipsLeft[0].y = 0; gSpriteClipsLeft[0].w = 134; gSpriteClipsLeft[0].h = 99;
+		gSpriteClipsLeft[1].x = 4; gSpriteClipsLeft[1].y = 105; gSpriteClipsLeft[1].w = 134; gSpriteClipsLeft[1].h = 99;
+		gSpriteClipsLeft[2].x = 4; gSpriteClipsLeft[2].y = 210; gSpriteClipsLeft[2].w = 134; gSpriteClipsLeft[2].h = 99;
+		gSpriteClipsLeft[3].x = 4; gSpriteClipsLeft[3].y = 315; gSpriteClipsLeft[3].w = 134; gSpriteClipsLeft[3].h = 99;
 	}
+
+	if (!gSeederMiniIconTexture.loadFromFile("SeederIconMiniTexture.png")) {
+		printf("Failed to load right icon animation texture!\n ");
+		success = false;
+	}
+	else {
+		gSpriteClipsRight[0].x = 1; gSpriteClipsRight[0].y = 0; gSpriteClipsRight[0].w = 75; gSpriteClipsRight[0].h = 53;
+		gSpriteClipsRight[1].x = 1; gSpriteClipsRight[1].y = 55; gSpriteClipsRight[1].w = 75; gSpriteClipsRight[1].h = 53;
+		gSpriteClipsRight[2].x = 1; gSpriteClipsRight[2].y = 111; gSpriteClipsRight[2].w = 75; gSpriteClipsRight[2].h = 53;
+		gSpriteClipsRight[3].x = 1; gSpriteClipsRight[3].y = 168; gSpriteClipsRight[3].w = 75; gSpriteClipsRight[3].h = 53;
+	}
+
+	
 	//map
 	
 	if (!gMapLeft.loadFromFile("MapLeft.png")) {
+		printf("Failed to load map texture!\n");
+		success = false;
+	}
+
+	if (!gMapRight.loadFromFile("MapRight.png")) {
 		printf("Failed to load map texture!\n");
 		success = false;
 	}
@@ -320,6 +479,9 @@ bool loadMedia() {
 }
 
 void close() {
+	//Free loaded textures
+	gTimeTextTexture.free();
+	gPromptTextTexture.free();
 	//Free loaded image
 	SDL_DestroyTexture(gTexture);
 	gTextBlock.free();
@@ -327,10 +489,10 @@ void close() {
 	TTF_CloseFont(gFont);
 	gFont = NULL;
 	//destroy window
-	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyRenderer(gRendererMain);
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
-	gRenderer = NULL;
+	gRendererMain = NULL;
 
 
 	//Quit SDL subsystems
@@ -338,6 +500,8 @@ void close() {
 	TTF_Quit();
 	SDL_Quit();
 }
+
+
 
 SDL_Texture* loadTexture(std::string path)
 {
@@ -352,7 +516,7 @@ SDL_Texture* loadTexture(std::string path)
 	else
 	{
 		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		newTexture = SDL_CreateTextureFromSurface(gRendererMain, loadedSurface);
 		if (newTexture == NULL)
 		{
 			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
@@ -387,6 +551,19 @@ int main(int argc, char* args[]) {
 			bool quit = false;
 			//Event handler
 			SDL_Event e;
+			//Timer
+			SDL_Color textColor = { 0,0,0,255 };
+			Uint32 startTime = 0;
+			//In memory text stream
+			std::stringstream timeText;
+			//The dot
+			Dot dot;
+			//set the wall
+			SDL_Rect wall;
+			wall.x = 527;
+			wall.y = 5;
+			wall.w = 389;
+			wall.h = 560;
 			//Current rendered texture
 			LTexture* currentTexture = NULL;
 			int frame = 0;
@@ -398,17 +575,20 @@ int main(int argc, char* args[]) {
 			while (!quit) {
 				//handle events on queue
 				while (SDL_PollEvent(&e) != 0) {
-
+					
 					if (e.type == SDL_QUIT) {
 
 						quit = true;
 
 					}
-
+					else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN) {
+						startTime = SDL_GetTicks();
+					}
 					//Clear screen
-					SDL_RenderClear(gRenderer);
+					SDL_RenderClear(gRendererMain);
 					//Render texture to screen
-					SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+					SDL_RenderCopy(gRendererMain, gTexture, NULL, NULL);
+					
 					//Update screen
 
 					/////////////////////////////BACKGROUND//////////////////////////////////////////
@@ -419,11 +599,12 @@ int main(int argc, char* args[]) {
 					LeftViewer.y = 5;
 					LeftViewer.w = 517;
 					LeftViewer.h = 387;
-					SDL_RenderSetViewport(gRenderer, &LeftViewer);
+					SDL_RenderSetViewport(gRendererMain, &LeftViewer);
 
 					//Render texture to screen
 					gMapLeft.render(-200, -200, NULL, degrees, NULL, flipType);
 
+					
 					////////////////////////////////Rotating map//////////////////////////////
 					if (e.type == SDL_KEYDOWN) {
 						switch (e.key.keysym.sym) {
@@ -438,57 +619,90 @@ int main(int argc, char* args[]) {
 					}
 					gMapTexture.render((SCREEN_WIDTH - gMapTexture.getWidth() / 2),
 						(SCREEN_HEIGHT - gMapTexture.getHeight()) / 2, NULL, degrees, NULL, flipType);
+					//Draw blue line
+					SDL_SetRenderDrawColor(gRendererMain, 0x00, 0x00, 0xFF, 0xFF);
+					SDL_RenderDrawLine(gRendererMain, 0, 199, 524, 199);
+					SDL_RenderDrawLine(gRendererMain, 265.5, 0, 265.5, 392);
+
+					//////////////////////////////SEED RIGHT VIEW///////////////////////////////////
+					SDL_Rect RightViewer;
+					RightViewer.x = wall.x;
+					RightViewer.y = wall.y;
+					RightViewer.w = wall.w;
+					RightViewer.h = wall.h;
+				
+					SDL_RenderSetViewport(gRendererMain, &RightViewer);
+					gMapRight.render(0, 0, NULL, NULL, NULL, flipType);
 
 					///////////////////////////RENDERED SHAPES//////////////////////////////////////
-
-
-					//Draw blue line
-					SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
-					SDL_RenderDrawLine(gRenderer, 0, 199, 524, 199);
-					SDL_RenderDrawLine(gRenderer, 265.5, 0, 265.5, 392);
-
 					//vertical yellow dot line
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_SetRenderDrawColor(gRendererMain, 0xFF, 0xFF, 0xFF, 0xFF);
 
 					SDL_Rect BackViewer;
 					BackViewer.x = 0;
 					BackViewer.y = 0;
 					BackViewer.w = SCREEN_WIDTH;
 					BackViewer.h = SCREEN_HEIGHT;
-					SDL_RenderSetViewport(gRenderer, &BackViewer);
+					SDL_RenderSetViewport(gRendererMain, &BackViewer);
 					/////////////////////////Seeder Animation////////////////////////////////
 					//Set texture based on current keystate
+				
 					const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-					if (currentKeyStates[SDL_SCANCODE_UP]) {
+					if (currentKeyStates[SDL_SCANCODE_UP]||(currentKeyStates[SDL_SCANCODE_DOWN])) {
+						
 						//Render current frame
-						gSeederIcon = NULL;
-						SDL_Rect* currentClip = &gSpriteClips[frame / 4];
-						gSpriteSheetTexture.render(206, 120, currentClip);
+						gSeederIconLeft = NULL;
+						SDL_Rect* iconLeft = &gSpriteClipsLeft[frame / 4];
+						gSeederIconRight = NULL;
+						SDL_Rect* iconRight = &gSpriteClipsRight[frame / 4];
+						//dot.render(false);
+						gSeederIconTexture.render(206, 120, iconLeft);
+						gSeederMiniIconTexture.render(dot.mPosX+RightViewer.x,dot.mPosY,iconRight);
 						SDL_Rect icon;
 						//go to next frame
 						++frame;
-
 						//cycle animation
 						if (frame / 4 >= WALKING_ANIMATION_FRAMES) {
 							frame = 0;
-						}
-					} else {
-						gSeederIcon = loadTexture("SeederIcon2.png");
+						} 
 					}
+					else {
+						gSeederIconLeft = loadTexture("SeederIcon2.png");
+						gSeederIconRight = loadTexture("SeederIconMini.png");
+	
+					}
+					
 					/////////////////////////////Seeder Icon left////////////////////////////////////
-					SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+					SDL_RenderCopy(gRendererMain, gTexture, NULL, NULL);
 					SDL_Rect iconleft;
 					iconleft.x = 205;
 					iconleft.y = 120;
 					iconleft.w = 135;
 					iconleft.h = 99;
-					SDL_RenderSetViewport(gRenderer, &iconleft);
-					SDL_RenderCopy(gRenderer, gSeederIcon, NULL, NULL);
+					SDL_RenderSetViewport(gRendererMain, &iconleft);
+					SDL_RenderCopy(gRendererMain, gSeederIconLeft, NULL, NULL);
 					SDL_UpdateWindowSurface(gWindow);
+					SDL_Rect iconRight;
+					iconRight.x = dot.mPosX + RightViewer.x;
+					iconRight.y = dot.mPosY;
+					iconRight.w = 70;
+					iconRight.h = 54;
+					SDL_RenderSetViewport(gRendererMain, &iconRight);
+					SDL_RenderCopy(gRendererMain, gSeederIconRight, NULL, NULL);
+					
+					SDL_UpdateWindowSurface(gWindow);
+					//////////////////////////////////Dot//////////////////////////////////////////////
+					dot.handleEvent(e);
+					dot.move(wall);
 				}
-					SDL_RenderPresent(gRenderer);
+				//render wall
+				SDL_RenderDrawRect(gRendererMain, &wall);
+				//render dot
+					//DOT
+					dot.render(!quit);
+					SDL_RenderPresent(gRendererMain);
 					//Update the surface
-					SDL_UpdateWindowSurface(gWindow);
+					//SDL_UpdateWindowSurface(gWindow);
 				}
 			}
 			close();
